6a52b18de8dbf15755e4bdaa9500f92ea0f8d461
Fahmi Adam <mohammad@fahmi.my>
2018-05-10 01:15:10 +0800

    one liner

---
+++ technique/one-liner.js
@@ -0,0 +1,7 @@
+// max
+const max = (arr) => Math.max(...arr);
+console.log(max([123, 321, 231]));
+
+// sum
+const sum = (arr) => arr.reduce((a, b) => (a + b), 0);
+console.log(sum([1, 2, 3, 5, 8, 13]));
