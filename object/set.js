7d993601aabc8cdaa8751611f6aab0be0bf93ed2
Fahmi Adam <mohammad@fahmi.my>
2018-05-13 19:37:58 +0800

    buildin object: set

---
+++ object/set.js
@@ -0,0 +1,12 @@
+const set = new Set([1, 2, 3, 4, 5]);
+
+console.log(set.has(1));
+// expected: true
+
+console.log(set.has(5));
+// expected: true
+
+console.log(set.has(9));
+// expected: false
+
+
